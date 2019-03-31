dfdc7aaefc655bed903fe819e7dccc669aa6fb80
Karl John <7484848+karloberas@users.noreply.github.com>
2019-03-31 22:36:25 +0800

    connected signup to backend

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Login() {
-    return <h1>Login</h1>
+    return (
+        <>
+            <h1>Login</h1>
+            <div className="auth-form">
+                <form>
+                    <input className="form-input" type="email" placeholder="Email" />
+                    <input className="form-input" type="passwprd" placeholder="Password" />
+                    <input className="form-submit" type="submit" value="Login" />
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default Login;
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'redux-react-hook';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../constants/action_types';
+import * as routes from '../../constants/routes';
 
-function Signup() {
-    return <h1>Sign up</h1>
+function Signup(props) {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+
+    const handleChange = setter => e =>  {
+        setter(e.target.value);
+    }
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const requestBody = {
+                query: `mutation {
+                    createUser(userInput: {
+                        email: "${email}",
+                        password: "${password}",
+                        confirm: "${confirm}"
+                    }) {
+                        _id,
+                        token,
+                        email
+                    }
+                }`
+            };
+
+            const { data } = await axios.post('http://localhost:3001/graphql', requestBody);
+
+            if (data.errors) {
+                console.log(data.errors[0].message);
+            }
+            else {
+                const { _id, token } = await data.data.createUser;
+
+                // dispatch value and set new value for the authUser
+                dispatch({
+                    type: actions.SET_AUTH_USER,
+                    authUser: {
+                        _id,
+                        email
+                    }
+                });
+
+                // store token into our localStorage
+                localStorage.setItem('token', token);
+
+                // redirect url to homepage
+                props.history.push(routes.HOME);
+            }
+
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <>
+            <h1>Sign up</h1>
+            <div className="auth-form">
+                <form onSubmit={submit}>
+                    <input className="form-input" type="email" placeholder="Email" value={email} onChange={handleChange(setEmail)} />
+                    <input className="form-input" type="password" placeholder="Password" value={password} onChange={handleChange(setPassword)} />
+                    <input className="form-input" type="password" placeholder="Confirm Password" value={confirm} onChange={handleChange(setConfirm)} />
+                    <input className="form-submit" type="submit" value="Register" />
+                </form>
+            </div>
+        </>
+    );
 }
 
-export default Signup;
+export default withRouter(Signup);
